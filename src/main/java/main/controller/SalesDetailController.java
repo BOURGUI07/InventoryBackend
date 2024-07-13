1b16d71b1af7835d62efdb4c71c3c51433d21d0a
Youness Bourgui <younessbourgui07@gmail.com>
2024-07-13 18:11:43 +0200

    Create The Detail Controllers

---
+++ src/main/java/main/controller/SalesDetailController.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.controller;
+
+import jakarta.validation.Valid;
+import java.util.List;
+import main.dto.SalesDetailDTO;
+import main.handler.RessourceNotFoundException;
+import main.service.SalesDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author hp
+ */
+@RestController
+@RequestMapping("/api/salesdetails")
+@Validated
+public class SalesDetailController{
+    @Autowired
+    public SalesDetailController(SalesDetailService service) {
+        this.service = service;
+    }
+    private final SalesDetailService service;
+    
+    @GetMapping
+    public ResponseEntity<List<SalesDetailDTO>> findAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
+    
+    @GetMapping("/{productid}/{salesid}")
+    public ResponseEntity<SalesDetailDTO> findById(@PathVariable Integer productid,@PathVariable Integer salesid ){
+        if(productid<0 || salesid<0){
+            throw new RessourceNotFoundException("Id non-valid");
+        }
+        if(service.findById(productid,salesid)==null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(service.findById(productid,salesid));
+    }
+    
+    
+    @PostMapping
+    public ResponseEntity<SalesDetailDTO> create(@Valid @RequestBody SalesDetailDTO x){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(x));
+    }
+    
+    @PutMapping("/{productid}/{salesid}")
+    public ResponseEntity<SalesDetailDTO> update(@PathVariable Integer productid,@PathVariable Integer salesid , @Valid @RequestBody SalesDetailDTO x){
+        if(productid<0 || salesid<0){
+            throw new RessourceNotFoundException("Id non-valid");
+        }
+        if(service.findById(productid,salesid)==null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(service.update(productid,salesid, x));
+    }
+    
+    
+    @DeleteMapping("/{productid}/{salesid}")
+    public ResponseEntity<Void> delete(@PathVariable Integer productid, @PathVariable Integer salesid){
+        if(productid<0 || salesid<0){
+            throw new RessourceNotFoundException("Id non-valid");
+        }
+        if(service.findById(productid,salesid)==null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        service.delete(productid,salesid);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/main/controller/SuppOrderDetailController.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.controller;
+
+import jakarta.validation.Valid;
+import java.util.List;
+import main.dto.SuppOrderDetailDTO;
+import main.handler.RessourceNotFoundException;
+import main.service.SuppOrderDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author hp
+ */
+@RestController
+@RequestMapping("/api/suppdetails")
+@Validated
+public class SuppOrderDetailController {
+    @Autowired
+    public SuppOrderDetailController(SuppOrderDetailService service) {
+        this.service = service;
+    }
+    private final SuppOrderDetailService service;
+    
+    @GetMapping
+    public ResponseEntity<List<SuppOrderDetailDTO>> findAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
+    
+    @GetMapping("/{productid}/{orderid}")
+    public ResponseEntity<SuppOrderDetailDTO> findById(@PathVariable Integer productid,@PathVariable Integer orderid ){
+        if(productid<0 || orderid<0){
+            throw new RessourceNotFoundException("Id non-valid");
+        }
+        if(service.findById(productid,orderid)==null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(service.findById(productid,orderid));
+    }
+    
+    
+    @PostMapping
+    public ResponseEntity<SuppOrderDetailDTO> create(@Valid @RequestBody SuppOrderDetailDTO x){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(x));
+    }
+    
+    @PutMapping("/{productid}/{orderid}")
+    public ResponseEntity<SuppOrderDetailDTO> update(@PathVariable Integer productid,@PathVariable Integer orderid , @Valid @RequestBody SuppOrderDetailDTO x){
+        if(productid<0 || orderid<0){
+            throw new RessourceNotFoundException("Id non-valid");
+        }
+        if(service.findById(productid,orderid)==null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(service.update(productid,orderid, x));
+    }
+    
+    
+    @DeleteMapping("/{productid}/{orderid}")
+    public ResponseEntity<Void> delete(@PathVariable Integer productid, @PathVariable Integer orderid){
+        if(productid<0 || orderid<0){
+            throw new RessourceNotFoundException("Id non-valid");
+        }
+        if(service.findById(productid,orderid)==null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        service.delete(productid,orderid);
+        return ResponseEntity.noContent().build();
+    }
+}
